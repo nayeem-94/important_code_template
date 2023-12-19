d672a46abf0cc29e1fa508c2a6d3cbdc19ba996b
MOSTAFIZUR RAHMAN NAYEEM <111490734+nayeem-94@users.noreply.github.com>
2023-12-19 23:14:37 +0600

    Create code_template.cpp

---
+++ code_template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define newline cout<<'\n';
+#define space cout<<' ';
+#define print(s) cout<<s;
+#define printarry(a) for(auto it : a) cout<<it<<' ';
+#define crack(a) cout<<"this is"<<" ----> "<<a;
+#define dbg(x) cout<<"line " << __LINE__ << " says: " << #x << " = " << x << "\n";
+using namespace std;
+
+int main()
+{
+  
+}
